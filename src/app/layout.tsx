a76a5d86f3bae8f7bff002e46015df34be6c86ce
Elias Soares <esoares@iget.com.br>
2023-10-31 17:12:18 -0300

    fix(layout): wrong container sizing causing missing background after scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className + ' relative'}>
         <div className="absolute bg-red-900 top-0 left-0 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] w-full h-full opacity-20 pointer-events-none">
         </div>
-        <div className="mx-auto max-w-7xl pt-4 px-4 sm:px-6 lg:px-8 bg-white dark:bg-slate-800 min-h-screen">
+        <div className="mx-auto max-w-7xl pb-6 pt-4 px-4 sm:px-6 lg:px-8 bg-white dark:bg-slate-800 min-h-screen">
             {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
             <div className="mx-auto max-w-5xl">{children}</div>
         </div>
